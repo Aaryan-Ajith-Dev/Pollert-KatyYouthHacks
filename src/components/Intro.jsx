6d0e5d5a7d259b50d28e4066566a004cf53c6f3f
Aaryan-Ajith-Dev <117074091+Aaryan-Ajith-Dev@users.noreply.github.com>
2023-08-13 16:21:55 +0530

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';  // Import useState
-import { Icon } from '@iconify/react';
+import { Icon } from '@iconify/react'; 
+import { Link } from 'react-router-dom';
 
 function Intro() {
   const [errorMessage, setErrorMessage] = useState("");  // State to manage error message
   const apiKey = "98d48a25b83c30f992bfc51a8115a841be143718";
   const apiUrl = "https://api.waqi.info/feed/";
+  const [aqi,setAQI]=useState(null)
   
   async function checkWeather(city) {
     try {
@@ -15,10 +17,16 @@ function Intro() {
       }
       const data = await response.json();
       // AQI VALUE IS ADDED HERE ----------------------------------------------
-      document.querySelector(".city").innerHTML = data['data']['aqi']; 
+      if (data['status'] === 'ok')
+        setAQI(data['data']['aqi'])
+      else {
+        setAQI(null)
+        alert('Enter a valid city')
+      }
       // ----------------------------------------------------------------------
     } catch (error) {
       setErrorMessage("An error occurred. Please try again.");  // Set error message
+      console.log(error)
     }
   }
 
@@ -26,6 +34,37 @@ function Intro() {
     const searchBox = document.querySelector(".search-text");
     checkWeather(searchBox.value);
   };
+
+  let to = '';
+  let state = null;
+  if (aqi === null) {
+    to=''
+    state = null
+  }
+  else if (aqi <= 50) {
+    to = '/level-1';
+    state = 'Good'
+  }
+  else if (aqi <= 100) {
+    to = '/level-2';
+    state = 'Moderate'    
+  }
+  else if (aqi <= 150) {
+    to = '/level-3';
+    state = 'Unhealthy for Sensitive Groups'    
+  }
+  else if (aqi <= 200) {
+    to = '/level-4';
+    state = 'Unhealthy'    
+  }
+  else if (aqi <= 300) {
+    to = '/level-5';
+    state = 'Very Unhealthy'    
+  }
+  else {
+    to = '/level-6'
+    state = 'Hazardous'
+  }
 
   return (
     <div className="header" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60')" }}>
@@ -37,9 +76,9 @@ function Intro() {
           <Icon icon="ion:search" color="white" style={{ transform: "rotate(90deg)" }} />
         </button>
       </div>
-      <div className="weather">       
-        <h2 className="city">New York</h2>
+      <div className="weather">   
         <p className="error">{errorMessage}</p>
+        <Link to={to}>{state}</Link>
       </div>
     </div>
   );
