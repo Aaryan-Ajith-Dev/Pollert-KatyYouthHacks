3b76f2f19a0f6241c155c146daff9708846a6319
Aaryan-Ajith-Dev <117074091+Aaryan-Ajith-Dev@users.noreply.github.com>
2023-08-13 19:01:50 +0530

    Update Level.jsx

--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -4,44 +4,40 @@ import { useState } from 'react'
 
 export default function Level({name}){
     const [show,setShow]=useState(false);
-    return(
-        <>
-            <button className='nav-item' onClick={()=>setShow((prev)=>!prev)}
+    return (
+        <div className="dropdown">
+            <button className='dropbtn' onClick={()=>setShow((prev)=>!prev)}
             >{name}</button>
             {show &&
-            <ul>
-                <li>
+            <div className="dropdown-content">
                     <Link to='/level-1'>
                      Good
                     </Link>
-                </li>
-                <li>
+
                     <Link to='/level-2'>
                      Moderate
                     </Link>
-                </li>
-                <li>
+
                     <Link to='/level-3'>
                     Unhealthy for Sensitive Groups
                     </Link>
-                </li>
-                <li>
+
                     <Link to='/level-4'>
                     Unhealthy
                     </Link>
-                </li>
-                <li>
+                
+                
                     <Link to='/level-5'>
                     Very Unhealthy
                     </Link>
-                </li>
-                <li>
+                
+                
                     <Link to='/level-6'>
                     Hazardous
                     </Link>
-                </li>
-            </ul>
+            </div>
             }
-        </>
+        </div>
+
     )
 }
