eade1c6157a01b1ce5eba1e9173f0b760d32f9fc
Aaryan-Ajith-Dev <117074091+Aaryan-Ajith-Dev@users.noreply.github.com>
2023-08-13 15:21:48 +0530

    Updated Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,34 +3,17 @@ import { Link } from 'react-router-dom'
 import Level from './Level'
 
 function Navbar() {
-  const winSize = 737
   return (
     <div className='navbar'>
       <div className='nav-items'>
         <Link to='/' className='nav-item'>Home</Link>
-        <Level name='Level' />
-        <Link to='/About' className='nav-item'>About Us</Link>
-        <Link to='/Donate' className='nav-item'>Donate</Link>
-      </div> 
+        <Level name='Levels' />
+        <a className='nav-item' href='/#About'>About Us</a>
+        <a className='nav-item' href='/#Donate'>Donate</a>
+      </div>
     </div>
     )
 }
 
 export default Navbar
 
-// import React from 'react'
-
-// function Navbar() {
-//   return (
-//     <div className='navbar'>
-//       <div className='nav-items'>
-//         <div className='nav-item'>Home</div>
-//         <div className='nav-item'>Level</div>
-//         <div className='nav-item'>About Us</div>
-//         <div className='nav-item'>Donate</div>
-//       </div>    
-//     </div>
-//     )
-// }
-
-// export default Navbar
