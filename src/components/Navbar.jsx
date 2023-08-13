5d942013f419154090850c37f1c8669f019bb841
Aaryan-Ajith-Dev <117074091+Aaryan-Ajith-Dev@users.noreply.github.com>
2023-08-13 11:11:48 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Level from './Level'
 
 function Navbar() {
+  const winSize = 737
   return (
     <div className='navbar'>
       <div className='nav-items'>
-        <div className='nav-item'>Home</div>
-        <div className='nav-item'>Level</div>
-        <div className='nav-item'>About Us</div>
-        <div className='nav-item'>Donate</div>
-      </div>    
+        <Link to='/' className='nav-item'>Home</Link>
+        <Level name='Level' />
+        <Link to='/About' className='nav-item'>About Us</Link>
+        <Link to='/Donate' className='nav-item'>Donate</Link>
+      </div> 
     </div>
     )
 }
 
 export default Navbar
+
+// import React from 'react'
+
+// function Navbar() {
+//   return (
+//     <div className='navbar'>
+//       <div className='nav-items'>
+//         <div className='nav-item'>Home</div>
+//         <div className='nav-item'>Level</div>
+//         <div className='nav-item'>About Us</div>
+//         <div className='nav-item'>Donate</div>
+//       </div>    
+//     </div>
+//     )
+// }
+
+// export default Navbar
